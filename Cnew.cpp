8a173f67d056e7dcab65de22bf9040c5d8aed9df
MO7AM4D19 <anismohamed@std.mans.edu.eg>
2022-10-14 00:18:11 +0200

    orgnising files

--- Cnew.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-    std::cout <<"My name is mohamed"<<std::endl;
-    return 0;
-}
--- training/storageClasses3/main.c
+++
@@ -1,9 +0,0 @@
-int i = 0;
-void display();
-void main()
-{
-    for(;i<4;i++)
-    {
-        display();
-    }
-}
